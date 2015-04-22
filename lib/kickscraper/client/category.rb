5e3a2fce37744338ee55c33fe2ee323640e8cc5a
Tom King <4tking@gmail.com>
2015-04-22 12:44:08 -0700

    wip

--- lib/kickscraper/client/category.rb
+++ lib/kickscraper/client/category.rb
@@ -14,13 +14,13 @@ module Kickscraper
             return [] unless @projects || self.urls.web.discover
             api_url = self.urls.web.discover
             if page.present?
-                # api_url = "#{api_url}?page=#{page}&state=live"
-                @projects ||=  process_api_call(request_for, additional_path, search_terms = "state=live", page = page)
-                # @projects ||= Kickscraper.client.process_api_url("Projects", api_url)
+                api_url = "#{api_url}?page=#{page}&state=live"
+                # @projects ||=  process_api_call(request_for, additional_path, search_terms = "state=live", page = page)
+                @projects ||= Kickscraper.client.process_api_url("Projects", api_url)
             else
-                # api_url = "#{api_url}?state=live"
-                @projects ||= process_api_call(request_for, additional_path, search_terms = "state=live", page = page)
-                # @projects ||= Kickscraper.client.process_api_url("Projects", api_url)
+                api_url = "#{api_url}?state=live"
+                # @projects ||= process_api_call(request_for, additional_path, search_terms = "state=live", page = page)
+                @projects ||= Kickscraper.client.process_api_url("Projects", api_url)
             end
         end
     end
