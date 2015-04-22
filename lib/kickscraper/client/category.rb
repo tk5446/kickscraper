b6f72082113ec9eb55caee03c043c654b8866c2e
Tom King <4tking@gmail.com>
2015-04-22 12:31:29 -0700

    pagination

--- lib/kickscraper/client/category.rb
+++ lib/kickscraper/client/category.rb
@@ -14,12 +14,14 @@ module Kickscraper
             return [] unless @projects || self.urls.web.discover
             api_url = self.urls.web.discover
             if page.present?
-                api_url = "#{api_url}?page=#{page}&state=live"
+                # api_url = "#{api_url}?page=#{page}&state=live"
+                @projects ||=  process_api_call(request_for, additional_path, search_terms = "state=live", page = page)
+                # @projects ||= Kickscraper.client.process_api_url("Projects", api_url)
             else
-                api_url = "#{api_url}?state=live"
+                # api_url = "#{api_url}?state=live"
+                @projects ||= process_api_call(request_for, additional_path, search_terms = "state=live", page = page)
+                # @projects ||= Kickscraper.client.process_api_url("Projects", api_url)
             end
-            puts "#{api_url}"
-            @projects ||= Kickscraper.client.process_api_url("Projects", api_url)
         end
     end
 end
